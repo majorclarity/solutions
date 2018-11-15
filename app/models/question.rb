60c5fd76def48f12d9630bbc8634cc01f323a8cc
Branson Lilburn <branson@simplethread.com>
2018-11-14 19:51:59 -0600

    most_answered_question is class method, not instance method

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ApplicationRecord
   validates_presence_of :title
 
   # TODO write and test this method
-  #def most_answered_question
+  #def self.most_answered_question
   #end
 end
